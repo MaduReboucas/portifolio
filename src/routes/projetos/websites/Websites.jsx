4ecd0f1a8758eae9348663759be74c29dfb9fcc5
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-22 19:56:47 -0300

    base de projetos/websites

--- src/routes/projetos/websites/Websites.jsx
+++ src/routes/projetos/websites/Websites.jsx
@@ -1,7 +1,97 @@
 import "./Websites.css";
+import Navbar from "../../../components/navbar/Navbar";
 
-const Websites = () => {
-  return <div>Websites</div>;
+const Interfaces = () => {
+  return (
+    <div id="Websites">
+      <Navbar />
+      <div className="maintitle-w">
+        <div className="white">
+          <h1>PROJETOS</h1>
+        </div>
+        <h1 className="gray">WEBSITES</h1>
+      </div>
+      <div className="websites-container">
+        <div className="websites-line">
+          <div className="website margin">
+            <a href="https://github.com/MaduReboucas/to-do-list">
+              <div className="todolist"></div>
+              <div className="descirption-w ">
+                <h1>To do List</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+
+          <div className="website ">
+            <a href="https://github.com/MaduReboucas/contagem-regressiva">
+              <div className="cr"></div>
+              <div className="descirption-w">
+                <h1>Contagem Regressiva</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="websites-line">
+          <div className="website margin">
+            <a href="https://github.com/MaduReboucas/react-axios">
+              <div className="blog"></div>
+              <div className="descirption-w ">
+                <h1>Blog</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+
+          <div className="website ">
+            <a href="https://github.com/MaduReboucas/party-time">
+              <div className="pt"></div>
+              <div className="descirption-w">
+                <h1>Party Time</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="websites-line">
+          <div className="website margin">
+            <a href="https://github.com/MaduReboucas/memories">
+              <div className="memory"></div>
+              <div className="descirption-w ">
+                <h1>Memories</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+
+          <div className="website ">
+            <a href="https://github.com/MaduReboucas/avaliador-ts">
+              <div className="sdv"></div>
+              <div className="descirption-w">
+                <h1>Sistema de Avaliação</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="websites-line">
+          <div className="website ">
+            <a href="">
+              <div className="gf"></div>
+              <div className="descirption-w">
+                <h1>Github Finder</h1>
+                <p>Projeto - Udemy</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Websites;
+export default Interfaces;
