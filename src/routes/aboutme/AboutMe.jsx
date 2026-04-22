d86a40e11b91b7520c4b6707f470c91547dd697f
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-22 15:21:41 -0300

    Css Sobre mim

--- src/routes/aboutme/AboutMe.jsx
+++ src/routes/aboutme/AboutMe.jsx
@@ -10,17 +10,18 @@ const AboutMe = () => {
   return (
     <div className="aboutme">
       <Navbar />
+
       <div className="maintitle-container">
         <div className="maintitle">
-          <h1>
+          <h1 className="normal-color">
             SOBRE <br /> MIM
           </h1>
-          <p>Oi, eu sou a Madu!</p>
+          <p className="degrade">Oi, eu sou a Madu!</p>
         </div>
-        <img src={meImage} alt="foto-minha" />
+        <img src={meImage} alt="foto-minha" id="me-img" />
       </div>
       <div className="description">
-        <p>
+        <p className="degrade">
           Sou desenvolvedora front-end em formação, com foco em React e na
           criação de interfaces modernas e responsivas. Tenho experiência com
           HTML, CSS, JavaScript, TypeScript e React, além de conhecimento em
@@ -30,64 +31,57 @@ const AboutMe = () => {
         </p>
       </div>
       <div className="skils-containers">
-        <h1>COMPETÊNCIAS</h1>
+        <h1 className="normal-color">COMPETÊNCIAS</h1>
         <div className="skils">
           <div className="line-skils">
             <ul>
               <li>
-                <a href="">HTML</a>
+                <a
+                  className="normal-color"
+                  href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
+                >
+                  HTML
+                </a>
               </li>
               <li>
-                <a href="">CSS</a>
+                <a
+                  className="normal-color"
+                  href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+                >
+                  CSS
+                </a>
               </li>
               <li>
-                <a href="">JavaScript</a>
+                <a
+                  className="normal-color"
+                  href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+                >
+                  JavaScript
+                </a>
               </li>
               <li>
-                <a href="">TypeScript</a>
+                <a
+                  className="normal-color"
+                  href="https://developer.mozilla.org/en-US/docs/Glossary/TypeScript"
+                >
+                  TypeScript
+                </a>
               </li>
               <li>
-                <a href="">React</a>
+                <a
+                  className="normal-color"
+                  href="https://developer.mozilla.org/pt-BR/docs/Learn_web_development/Core/Frameworks_libraries/React_getting_started"
+                >
+                  React
+                </a>
               </li>
               <li>
-                <a href="">Figma</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="line-skils">
-            <ul>
-              <li>
-                <a href="">Canva</a>
-              </li>
-              <li>
-                <a href="">Microsoft Word</a>
-              </li>
-              <li>
-                <a href="">Microsoft Excel</a>
-              </li>
-              <li>
-                <a href="">Github</a>
-              </li>
-              <li>
-                <a href="">Cypress</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="line-skils">
-            <ul>
-              <li>
-                <a href="">Social Media</a>
-              </li>
-              <li>
-                <a href="">Manual Testing</a>
-              </li>
-              <li>
-                <a href="">Teste Automation</a>
-              </li>
-              <li>
-                <a href="">Web Design</a>
+                <a
+                  className="normal-color"
+                  href="https://help.figma.com/hc/pt-br/articles/14563969806359-O-que-%C3%A9-o-Figma"
+                >
+                  Figma
+                </a>
               </li>
             </ul>
           </div>
@@ -95,10 +89,102 @@ const AboutMe = () => {
           <div className="line-skils">
             <ul>
               <li>
-                <a href="">Marketing Digital</a>
+                <a
+                  className="normal-color"
+                  href="https://www.canva.com/pt_br/about/"
+                >
+                  Canva
+                </a>
               </li>
               <li>
-                <a href="">VS Code</a>
+                <a
+                  className="normal-color"
+                  href="https://support.microsoft.com/pt-br/word"
+                >
+                  Microsoft Word
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://support.microsoft.com/pt-br/office/o-que-%C3%A9-o-excel-94b00f50-5896-479c-b0c5-ff74603b35a3"
+                >
+                  Microsoft Excel
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://docs.github.com/pt/get-started/start-your-journey/about-github-and-git"
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://www.softdesign.com.br/blog/cypress_passo_a_passo_para_comecar_a_usar/"
+                >
+                  Cypress
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="line-skils">
+            <ul>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://www.rdstation.com/blog/marketing/o-que-faz-um-social-media/"
+                >
+                  Social Media
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://www.zaptest.com/pt-br/testes-manuais-o-que-e-tipos-processos-abordagens-ferramentas-mais"
+                >
+                  Manual Testing
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://www.servicenow.com/br/platform/workflow-data-fabric/what-is-test-automation.html"
+                >
+                  Teste Automation
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://pm3.com.br/blog/web-design/"
+                >
+                  Web Design
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="line-skils">
+            <ul>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://www.rdstation.com/blog/marketing/marketing-digital/"
+                >
+                  Marketing Digital
+                </a>
+              </li>
+              <li>
+                <a
+                  className="normal-color"
+                  href="https://learn.microsoft.com/pt-br/shows/visual-studio-code/"
+                >
+                  VS Code
+                </a>
               </li>
             </ul>
           </div>
