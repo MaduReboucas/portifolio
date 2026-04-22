11b4db3e2721c6686b63781d26606141ebfcb4ef
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-22 18:54:38 -0300

    Css interfaces

--- src/routes/projetos/interfaces/Interfaces.jsx
+++ src/routes/projetos/interfaces/Interfaces.jsx
@@ -1,56 +1,68 @@
 import "./Interfaces.css";
 import Navbar from "../../../components/navbar/Navbar";
 
-// images pb
-import acdcPb from "../img/pb/img-acdc.png";
-import apmPb from "../img/pb/img-apm.png";
-import sdvPb from "../img/pb/img-sdv.png";
-import soadPb from "../img/pb/img-soad.png";
-import stPb from "../img/pb/img-st.png";
-
 const Interfaces = () => {
   return (
     <div id="Interfaces">
       <Navbar />
       <div className="maintitle-i">
-        <h1>
-          PROJETOS <span>INTERFACES</span>
-        </h1>
+        <div className="white">
+          <h1>PROJETOS</h1>
+        </div>
+        <h1 className="gray">INTERFACES</h1>
       </div>
       <div className="interfaces-container">
         <div className="interfaces-line">
-          <div className="interface">
-            <img src={acdcPb} alt="acdc-image" />
-            <h1>AC/DC</h1>
-            <p>Interface para website de homenagem à banda AC/DC</p>
+          <div className="interface margin">
+            <div className="acdc-image"></div>
+            <div className="descirption-i ">
+              <h1>AC/DC</h1>
+              <p>
+                Interface para website de homenagem <br />à banda AC/DC
+              </p>
+            </div>
           </div>
 
-          <div className="interface">
-            <img src={apmPb} alt="apm-image" />
-            <h1>APM</h1>
-            <p>Atualização - Interface do website da Academia Pedro Martinez</p>
-          </div>
-        </div>
-
-        <div className="interfaces-line">
-          <div className="interface">
-            <img src={soadPb} alt="soad-image" />
-            <h1>SOAD</h1>
-            <p>Interface para website de homenagem à banda System of a Down</p>
-          </div>
-
-          <div className="interface">
-            <img src={stPb} alt="saTech-image" />
-            <h1>SaTech</h1>
-            <p>Interface de website para trabalho escolar </p>
+          <div className="interface ">
+            <div className="apm-image"></div>
+            <div className="descirption-i">
+              <h1>APM</h1>
+              <p>
+                Atualização - Interface do website da <br /> Academia Pedro
+                Martinez
+              </p>
+            </div>
           </div>
         </div>
 
         <div className="interfaces-line">
-          <div className="interface">
-            <img src={sdvPb} alt="sdv-image" />
-            <h1>Saia do Vermelho</h1>
-            <p>Interface de website para projeto escolar</p>
+          <div className="interface margin ">
+            <div className="soad-image"></div>
+            <div className="descirption-i ">
+              <h1>SOAD</h1>
+              <p>
+                Interface para website de homenagem à <br /> banda System of a
+                Down
+              </p>
+            </div>
+          </div>
+
+          <div className="interface ">
+            <div className="saTech-image"></div>
+            <div className="descirption-i">
+              <h1>SaTech</h1>
+              <p>Interface de website para trabalho escolar </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="interfaces-line">
+          <div className="interface ">
+            <div className="sdv-image"></div>
+            <div className="descirption-i">
+              <h1>Saia do Vermelho</h1>
+              <p>Interface de website para projeto escolar</p>
+            </div>
           </div>
         </div>
       </div>
