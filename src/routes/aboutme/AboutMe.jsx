aa39141c7c03ee6c6752571ef4ce3706da16e6ee
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-22 16:02:54 -0300

    Css sobre competências

--- src/routes/aboutme/AboutMe.jsx
+++ src/routes/aboutme/AboutMe.jsx
@@ -33,161 +33,153 @@ const AboutMe = () => {
       <div className="skils-containers">
         <h1 className="normal-color">COMPETÊNCIAS</h1>
         <div className="skils">
-          <div className="line-skils">
-            <ul>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
-                >
-                  HTML
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
-                >
-                  CSS
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
-                >
-                  JavaScript
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://developer.mozilla.org/en-US/docs/Glossary/TypeScript"
-                >
-                  TypeScript
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://developer.mozilla.org/pt-BR/docs/Learn_web_development/Core/Frameworks_libraries/React_getting_started"
-                >
-                  React
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://help.figma.com/hc/pt-br/articles/14563969806359-O-que-%C3%A9-o-Figma"
-                >
-                  Figma
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="line-skils">
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
+              >
+                HTML
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+              >
+                CSS
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+              >
+                JavaScript
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://developer.mozilla.org/en-US/docs/Glossary/TypeScript"
+              >
+                TypeScript
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://developer.mozilla.org/pt-BR/docs/Learn_web_development/Core/Frameworks_libraries/React_getting_started"
+              >
+                React
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://help.figma.com/hc/pt-br/articles/14563969806359-O-que-%C3%A9-o-Figma"
+              >
+                Figma
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://www.canva.com/pt_br/about/"
+              >
+                Canva
+              </a>
+            </li>
+          </ul>
 
-          <div className="line-skils">
-            <ul>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://www.canva.com/pt_br/about/"
-                >
-                  Canva
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://support.microsoft.com/pt-br/word"
-                >
-                  Microsoft Word
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://support.microsoft.com/pt-br/office/o-que-%C3%A9-o-excel-94b00f50-5896-479c-b0c5-ff74603b35a3"
-                >
-                  Microsoft Excel
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://docs.github.com/pt/get-started/start-your-journey/about-github-and-git"
-                >
-                  Github
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://www.softdesign.com.br/blog/cypress_passo_a_passo_para_comecar_a_usar/"
-                >
-                  Cypress
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="line-skils">
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://support.microsoft.com/pt-br/word"
+              >
+                Microsoft Word
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://support.microsoft.com/pt-br/office/o-que-%C3%A9-o-excel-94b00f50-5896-479c-b0c5-ff74603b35a3"
+              >
+                Microsoft Excel
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://docs.github.com/pt/get-started/start-your-journey/about-github-and-git"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://www.softdesign.com.br/blog/cypress_passo_a_passo_para_comecar_a_usar/"
+              >
+                Cypress
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://www.rdstation.com/blog/marketing/o-que-faz-um-social-media/"
+              >
+                Social Media
+              </a>
+            </li>
+          </ul>
 
-          <div className="line-skils">
-            <ul>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://www.rdstation.com/blog/marketing/o-que-faz-um-social-media/"
-                >
-                  Social Media
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://www.zaptest.com/pt-br/testes-manuais-o-que-e-tipos-processos-abordagens-ferramentas-mais"
-                >
-                  Manual Testing
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://www.servicenow.com/br/platform/workflow-data-fabric/what-is-test-automation.html"
-                >
-                  Teste Automation
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://pm3.com.br/blog/web-design/"
-                >
-                  Web Design
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="line-skils">
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://www.zaptest.com/pt-br/testes-manuais-o-que-e-tipos-processos-abordagens-ferramentas-mais"
+              >
+                Manual Testing
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://www.servicenow.com/br/platform/workflow-data-fabric/what-is-test-automation.html"
+              >
+                Teste Automation
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://pm3.com.br/blog/web-design/"
+              >
+                Web Design
+              </a>
+            </li>
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://www.rdstation.com/blog/marketing/marketing-digital/"
+              >
+                Marketing Digital
+              </a>
+            </li>
+          </ul>
 
-          <div className="line-skils">
-            <ul>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://www.rdstation.com/blog/marketing/marketing-digital/"
-                >
-                  Marketing Digital
-                </a>
-              </li>
-              <li>
-                <a
-                  className="normal-color"
-                  href="https://learn.microsoft.com/pt-br/shows/visual-studio-code/"
-                >
-                  VS Code
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="line-skils">
+            <li>
+              <a
+                className="normal-color anchor"
+                href="https://learn.microsoft.com/pt-br/shows/visual-studio-code/"
+              >
+                VS Code
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
