652eeec15c6a1e75432155a9817fc6360202f81f
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-22 12:17:07 -0300

    Individualizando css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,21 +3,29 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <a href="">
+      <a className="navbar-link" href="">
         <h1>Meu Portifólio</h1>
       </a>
-      <ul>
-        <li>
-          <a href="">PROJETOS</a>
+      <ul id="list-navbar">
+        <li id="li-navbar">
+          <a className="navbar-link" href="">
+            PROJETOS
+          </a>
         </li>
-        <li>
-          <a href="">COMPETÊNCIAS</a>
+        <li id="li-navbar">
+          <a className="navbar-link" href="">
+            COMPETÊNCIAS
+          </a>
         </li>
-        <li>
-          <a href="">SOBRE MIM</a>
+        <li id="li-navbar">
+          <a className="navbar-link" href="">
+            SOBRE MIM
+          </a>
         </li>
-        <li>
-          <a href="">CONTATO</a>
+        <li id="li-navbar">
+          <a className="navbar-link" href="">
+            CONTATO
+          </a>
         </li>
       </ul>
     </nav>
--- src/routes/aboutme/AboutMe.jsx
+++ src/routes/aboutme/AboutMe.jsx
@@ -1,7 +1,111 @@
 import "./AboutMe.css";
 
+// components
+import Navbar from "../../components/navbar/Navbar";
+
+// imagens
+import meImage from "../../img/me-image.png";
+
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return (
+    <div className="aboutme">
+      <Navbar />
+      <div className="maintitle-container">
+        <div className="maintitle">
+          <h1>
+            SOBRE <br /> MIM
+          </h1>
+          <p>Oi, eu sou a Madu!</p>
+        </div>
+        <img src={meImage} alt="foto-minha" />
+      </div>
+      <div className="description">
+        <p>
+          Sou desenvolvedora front-end em formação, com foco em React e na
+          criação de interfaces modernas e responsivas. Tenho experiência com
+          HTML, CSS, JavaScript, TypeScript e React, além de conhecimento em
+          design e usabilidade. Também trabalho com marketing digital, criando
+          conteúdo estratégico para mídias sociais. Busco oportunidades para
+          crescer na área e contribuir com projetos inovadores.
+        </p>
+      </div>
+      <div className="skils-containers">
+        <h1>COMPETÊNCIAS</h1>
+        <div className="skils">
+          <div className="line-skils">
+            <ul>
+              <li>
+                <a href="">HTML</a>
+              </li>
+              <li>
+                <a href="">CSS</a>
+              </li>
+              <li>
+                <a href="">JavaScript</a>
+              </li>
+              <li>
+                <a href="">TypeScript</a>
+              </li>
+              <li>
+                <a href="">React</a>
+              </li>
+              <li>
+                <a href="">Figma</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="line-skils">
+            <ul>
+              <li>
+                <a href="">Canva</a>
+              </li>
+              <li>
+                <a href="">Microsoft Word</a>
+              </li>
+              <li>
+                <a href="">Microsoft Excel</a>
+              </li>
+              <li>
+                <a href="">Github</a>
+              </li>
+              <li>
+                <a href="">Cypress</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="line-skils">
+            <ul>
+              <li>
+                <a href="">Social Media</a>
+              </li>
+              <li>
+                <a href="">Manual Testing</a>
+              </li>
+              <li>
+                <a href="">Teste Automation</a>
+              </li>
+              <li>
+                <a href="">Web Design</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="line-skils">
+            <ul>
+              <li>
+                <a href="">Marketing Digital</a>
+              </li>
+              <li>
+                <a href="">VS Code</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
